3c619a65a201356b9b2ab8d03c4bbfb4668f6eca
Himanshu <himanshubadera10398@gmail.com>
2022-02-15 18:32:48 -0600

    Initial commit

---
+++ readers/paths.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import datajoint as dj
+
+
+def get_ephys_root_data_dir():
+    root_data_dirs = dj.config.get('custom', {}).get('ephys_root_data_dir', None)
+
+    return root_data_dirs
+
+
+def get_session_directory(session_key: dict) -> str:
+    from .pipeline import session
+    session_dir = (session.SessionDirectory & session_key).fetch1('session_dir')
+
+    return session_dir
+
